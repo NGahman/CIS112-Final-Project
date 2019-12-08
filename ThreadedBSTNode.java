fa3628134a8704f8921c367e7d2e3a6e5e708234
Cadence✮ <yeroc8.git@gmail.com>
2019-12-08 18:01:34 -0500

    Fix ThreadedBSTNode

--- ThreadedBSTNode.java
+++ ThreadedBSTNode.java
@@ -5,7 +5,18 @@ import bookFiles.support.BSTNode;
 public class ThreadedBSTNode<T> extends BSTNode<T> {
     public boolean hasThread;
 
+    private ThreadedBSTNode<T> left;
+    private ThreadedBSTNode<T> right;
+
     public ThreadedBSTNode(T info) {
         super(info);
+    }
+
+    public void setLeft(ThreadedBSTNode<T> link) {
+        left = link;
+    }
+
+    public void setRight(ThreadedBSTNode<T> link) {
+        right = link;
     }
 }

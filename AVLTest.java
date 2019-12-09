cec6c731b440ba9cc5c3435071ebe35412bf6093
NGahman <48140671+NGahman@users.noreply.github.com>
2019-12-09 12:54:30 -0500

    Updated AVLTest

--- AVLTest.java
+++ AVLTest.java
@@ -3,16 +3,16 @@ public class AVLTest
    public static void main(String[] args)
    {
       AVLThreadedBST<Integer> avl = new AVLThreadedBST();
-      System.out.println(avl.add(5));
-      System.out.println(avl.add(2));
-      System.out.println(avl.add(6));
-      System.out.println(avl.add(4));
-      System.out.println(avl.add(7));
       System.out.println(avl.add(1));
+      System.out.println(avl.add(2));
       System.out.println(avl.add(3));
+      System.out.println(avl.add(4));
+      System.out.println(avl.add(5));
+      System.out.println(avl.add(6));
+      System.out.println(avl.add(7));
       System.out.println(avl.add(8));
-      System.out.println(avl.add(10));
       System.out.println(avl.add(9));
+      System.out.println(avl.add(10));
       
       System.out.println(avl.get(3));
       System.out.println(avl.get(6));
